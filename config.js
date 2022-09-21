9e09d210ba9eef48ed7ed13ff4c47570da1f8a3a
ItzRudro <rk@gmail.com>
2022-09-21 21:24:30 +0600

    hi

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 module.exports = {
-    token: process.env.TOKEN,
-    ytapi: process.env.YT_API,
+    token: '5653018114:AAG0I7zMlxrurGIkqO_l3DLyf9xGVgR9w_Q',
+    ytapi: 'AIzaSyA6eOYIL3IHC3ZHPmsj2KTtgZN1JNLQvhk',
     botname : 'Sad Cat'
 }
